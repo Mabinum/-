6027dd6ff8336cab3da48483ac1608cff6b46013
Mabin <mabinum9528@naver.com>
2024-03-08 16:48:08 +0900

    스타벅스 거의 완성

--- JS_folder/main.js
+++ JS_folder/main.js
@@ -63,12 +63,12 @@ window.addEventListener('scroll',function(){
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl,index){
   // gsap.to(요소,지속시간,옵션:,{});
-gsap.to(fadeEl, 0.4,{ 
-  opacity:1,
-  delay: (index+1)*0.7//0.7s, 1.4s, 2.1s, 2.8s
-});
-// console.log(fadeEls);
-// fadeEl.setAttribute('opacity',"`${index+1};`");
+  gsap.to(fadeEl, 0.4,{ 
+    opacity:1,
+    delay: (index+1)*0.7//0.7s, 1.4s, 2.1s, 2.8s
+  });
+  // console.log(fadeEls);
+  // fadeEl.setAttribute('opacity',"`${index+1};`");
 });
 // `${index+1}`이건 문자사용할때 사용
 
@@ -77,14 +77,81 @@ gsap.to(fadeEl, 0.4,{
 // new Swiper(요소.{});
 // 첫 번째 인수: 슬라이드 기능을 적용할 요소의 선택자
 // 두 번째 인수: 다양한 옵션을 객체 데이터로 전달(API 페이지참고)
-const swiper = new Swiper('.swiper', {
+new Swiper('.notice .swiper', {
   // Optional parameters
   direction: 'vertical', //수직 슬라이드
   loop: true, //반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
   autoplay: true //자동 재생 여부 
 });
-
-
-
-
 // swiper 사이트 들어가서 설정들을 보고 나중에 적용가능할듯
+
+// 프로모션 수평 슬라이드 가능
+new Swiper('.promotion .swiper', {
+  // Optional parameters
+  direction: 'horizontal', //수평 슬라이드(기본값)
+  loop: true, //반복 재생 여부, 1 -> 2 -> 3 -> 4 -> 다시 1
+  autoplay: { //자동 재생 여부 
+    delay: 5000 // 5초마다 슬라이드 바뀜(기본값 : 3000)
+  } ,
+  slidesPerView: 3,//한 번에 보여줄 슬라이드 개수 (기본값:1)
+  spaceBetween:10, //슬라이드 사이 여백(간격) px
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  pagination: { //페이지 번호 사용
+    el : '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable : true,
+  },
+  navigation: { //슬라이드의 이전/다음 버튼 사용
+    nextEl: '.promotion .swiper-button-next',
+    prevEl: '.promotion .swiper-button-prev',
+  },
+}); 
+
+// 프로모션 섹션 토글 기능
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleElBtn = document.querySelector('.toggle-promotion');
+const promotionToggleElIcon = promotionToggleElBtn.querySelector('.material-icons');
+
+// Quiz
+// 토글 버튼을 클릭했을 때 아래 기능을 실행
+// 프로모션 요소에 'hide'라는 클래스 값이 있으면 보임 처리!
+// ('hide')클래스를 제거하고 아이콘 모양을 'upload'로 설정
+// 그렇지 않으면 숨김 처리!
+// ('hide')클래스를 추가하고 아이콘 모양을 'download'로 설정
+
+promotionToggleElBtn.addEventListener('click',function(){
+  if (promotionEl.classList.contains('hide')) {
+    promotionEl.classList.remove('hide');
+    promotionToggleElIcon.textContent = 'upload';
+  } else {
+    promotionEl.classList.add('hide');
+    promotionToggleElIcon.textContent = 'download';
+  }
+});
+
+// 유튜브 섹션 위에 부유 요소 애니메이션 처리
+// gsap.to(요소,지속시간,옵션:{})
+// 옵션 참고: http://greensock.com/docs/v3/GSAP/gsap.to()
+gsap.to('.floating1',1.5,{
+  delay:2, //얼마나 늦게 애니메이션을 시작할 것이지 지연 시간을 설정
+  y:65, //수직으로 얼마나 움직일지 설정, transform:translateY(수치)
+  repeat: -1, //몇 번 반복할지 설정, -1은 무한 반복
+  yoyo: true, //한 번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut //타이밍 함수 적용, 느리게-빠르게-느리게
+});
+gsap.to('.floating2',2,{
+  delay:1, //얼마나 늦게 애니메이션을 시작할 것이지 지연 시간을 설정
+  x:70,
+  y:35, //수직으로 얼마나 움직일지 설정, transform:translateY(수치)
+  repeat: -1, //몇 번 반복할지 설정, -1은 무한 반복
+  yoyo: true, //한 번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut //타이밍 함수 적용, 느리게-빠르게-느리게
+});
+gsap.to('.floating3',2,{
+  delay:3, //얼마나 늦게 애니메이션을 시작할 것이지 지연 시간을 설정
+  x:60,
+  y:40, //수직으로 얼마나 움직일지 설정, transform:translateY(수치)
+  repeat: -1, //몇 번 반복할지 설정, -1은 무한 반복
+  yoyo: true, //한 번 재생된 애니메이션을 다시 뒤로 재생
+  ease: Power1.easeInOut //타이밍 함수 적용, 느리게-빠르게-느리게
+});
+// 지속시간,delay,y를 자유롭게 변경하여 적용하기
