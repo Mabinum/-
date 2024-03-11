f5e0e2f0e85da1b53c1371c1e15414bba6ba9c72
Mabin <mabinum9528@naver.com>
2024-03-11 12:34:20 +0900

    스타벅스 거의 완성

--- JS_folder/main.js
+++ JS_folder/main.js
@@ -155,3 +155,17 @@ gsap.to('.floating3',2,{
   ease: Power1.easeInOut //타이밍 함수 적용, 느리게-빠르게-느리게
 });
 // 지속시간,delay,y를 자유롭게 변경하여 적용하기
+
+// scrollmagic 사용
+// 그 외 ScrollMagic
+const spyEls = document.querySelectorAll('section.ScrollMagic');
+spyEls.forEach(function(spyEl){
+  new ScrollMagic.Scene({ //감시할 장면(scene)추가 및 옵션 지정
+    triggerElement:spyEl, //보여짐 여부를 감시할 요소를 지정
+    triggerHook:0.8, //화면의 80% 지점에서 보여짐 여부 감시(0~1사이 지정)
+  })
+  .setClassToggle(spyEl,'show') //요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller());//컨트롤러에 장면을 할당(필수!)
+  // 라이브러리에서 지정한 문법으로 깊게 이해 X
+});
+
